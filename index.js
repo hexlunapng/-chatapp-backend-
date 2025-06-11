8c2dc501445501ac714b33b58c3689b4138f5fe6
hexlunapng <hexishexomgcute@gmail.com>
2025-06-11 14:49:09 -0400

    i am fucking dumb 😭

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const http = require('http');
+const { Socket } = require("net");
+const { disconnect } = require("process");
 const { Server } = require('socket.io');
 
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-    res.send('error.luna.dev');
+app.use(express.static ('public'));
+
+
+io.on('connection'), (socket) => {
+ console.log('someone connected: ', socket.id);
+}
+
+socket.on('chat message', (msg) => {
+console.log('a user sent a message:',  msg);
+io.emit('chat message', msg);
+});
+
+socket.on('connection', (stream) => {
+  console.log('someone connected!');
 });
